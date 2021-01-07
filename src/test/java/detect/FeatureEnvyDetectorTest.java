604bc82a1aba5350cca51f8373f3b18c8e9c9851
Old Jin <40513499+TyrantJin@users.noreply.github.com>
2021-01-07 19:07:17 +0800

    Modify a little

--- src/test/java/detect/FeatureEnvyDetectorTest.java
+++ src/test/java/detect/FeatureEnvyDetectorTest.java
@@ -10,14 +10,10 @@ public class FeatureEnvyDetectorTest {
             String[] classPaths={"D:\\huawei\\HW\\testproject\\grinder-3.6\\lib\\"};
             String nnModelPath = "D:\\huawei\\HW\\FeatureEnvyModel.h5";
             String embeddingModelPath = "D:\\huawei\\HW\\bin2vec\\src\\test\\java\\resources\\new.bin";
-            long t1 = System.currentTimeMillis();
             FeatureEnvyDetector featureEnvyDetector = new FeatureEnvyDetector(sourceFilePaths,encodings,classPaths,nnModelPath,embeddingModelPath);
             featureEnvyDetector.detect();
             //还需要讲候选目标类中的非本项目类筛选掉
             featureEnvyDetector.displayResult();
-            long t2 = System.currentTimeMillis();
-            //30.5s
-            System.out.println(t2-t1);
         }catch(Exception e){
             e.printStackTrace();
         }
